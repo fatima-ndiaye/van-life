ea47f8353739dafa293fc2c8aed72fffcd25a323
fatima-xs <fatimanndiaye@gmail.com>
2023-06-20 18:14:38 +0100

    Create Vans.jsx

---
+++ src/Pages/Vans.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Vans() {
+    return (
+        <div className=''>
+            <h1>Vans Page goes here</h1>
+        </div>
+    )
+}
+export default Vans
