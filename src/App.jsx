f01b1555ba301106d217a7c491cdb1cb776e5365
fatima-xs <fatimanndiaye@gmail.com>
2023-06-20 18:16:39 +0100

    Update App.jsx
    Create a Route for the Vans page on the /vans route
     Add a Link in the nav bar to the Vans route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Vans from './Pages/Vans'
 
 function App() {
  
@@ -12,12 +13,14 @@ function App() {
         <nav className='flex'>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
     </header>
 
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
