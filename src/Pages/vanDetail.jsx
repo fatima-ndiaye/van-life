73f6555ce2d7bdf928e89e9079669a7957690aa5
fatima-xs <fatimanndiaye@gmail.com>
2023-06-21 16:17:05 +0100

    Data fetch: /api/vans/

--- src/Pages/vanDetail.jsx
+++ src/Pages/vanDetail.jsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {useParams} from 'react-router-dom'
 
 function VanDetail() 
 {
     const params = useParams()
+    useEffect(()=>{
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    },[params.id])
     return <h1>Van detail page goes here</h1>
 }
+
 export default VanDetail
