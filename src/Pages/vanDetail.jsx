16c8e4bc9b665073742b4c6f2ef7ac204bdf8530
fatima-xs <fatimanndiaye@gmail.com>
2023-06-21 17:36:41 +0100

    Update VanDetail.jsx
    Code addition: VanDetail and CSS changes

--- src/Pages/vanDetail.jsx
+++ src/Pages/vanDetail.jsx
@@ -4,12 +4,31 @@ import {useParams} from 'react-router-dom'
 function VanDetail() 
 {
     const params = useParams()
+    const [van, setVan] = useState(null)
     useEffect(()=>{
         fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setVan(data.vans))
     },[params.id])
-    return <h1>Van detail page goes here</h1>
+    return (
+        <div className="van-detail-container container">
+            {
+                van ? 
+                <div className="van-detail flex">
+                    <img src={van.imageUrl}/>
+                    <div className="van-detail-info flex">
+                        <i className={`van-type ${van.type} selected bg-accent`}>{van.type}</i>
+                        <h3 className="fs-h3">{van.name}</h3>
+                        <p className='fw-bold'>${van.price}<span className='fw-reg'>/day</span></p>
+                        <p>{van.description}</p>
+                        <button className="link-btn bg-accent">Rent this van</button>
+                    </div>
+                </div>
+                :
+                <h2>Loading ...</h2>
+            }
+        </div>
+    )
 }
 
 export default VanDetail
