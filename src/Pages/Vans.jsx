87a3c032faa7e9452b6d1e5c06ce5d3f08de86a4
fatima-xs <fatimanndiaye@gmail.com>
2023-06-21 08:42:32 +0100

    Update Van.jsx
    Fetch and map over the data to display it on the vans page.

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Vans() {
+
+    const [vans, setVans] = useState([])
+    useEffect(()=>{
+        fetch('/api/vans')
+            .then(res => res.json())
+            .then(data => 
+                setVans(data.vans))
+    },[])
+
+    const vanEl = vans.map(van =>{
+        return (
+        <div className='van' key={van.id}>
+            <img src={van.imageUrl} className='van-img' />
+            <div className='van-info'>
+                    <h3 className='van-name fs-h3'>{van.name}</h3>
+                    <p><span>{van.price}</span>/day</p>
+            </div>
+
+        </div>)
+    })
     return (
-        <div className=''>
-            <h1>Vans Page goes here</h1>
+        <div className='vans-container container'>
+            <h2>Explore our van options</h2>
+            <div className='vans-filters'></div>
+            <div className='vans-list'>
+                {vanEl}
+            </div>
+          
+            
         </div>
     )
 }
