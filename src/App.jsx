d8f141b547ae3a45a36dd90e3212d37ea695841b
fatima-xs <fatimanndiaye@gmail.com>
2023-06-21 07:25:10 +0100

    Create server.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Vans from './Pages/Vans'
+import "./server"
 
 function App() {
  
