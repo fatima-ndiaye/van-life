71df891453ccd2c563ccf93b7aa469f72b7d9546
fatima-xs <fatimanndiaye@gmail.com>
2023-06-21 15:28:36 +0100

    Code addition: VanDetail.jsx

--- src/Pages/vanDetail.jsx
+++ src/Pages/vanDetail.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import {useParams} from 'react-router-dom'
 
 function VanDetail() 
 {
+    const params = useParams()
     return <h1>Van detail page goes here</h1>
 }
 export default VanDetail
