7e42f15415ef899f39f60da448ed5bd917d301e3
fatima-xs <fatimanndiaye@gmail.com>
2023-06-22 07:55:31 +0100

    Code addition: Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,6 @@ function App() {
  
   return (
     <BrowserRouter>
-    <header className='flex container'>
-        <p className='ff-secondary fs-h2 fw-bold letter-spacing-3'>#VanLife</p>
-        <nav className='flex'>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-    </header>
 
       <Routes>
         <Route path='/' element={<Home />}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() 
+{
+    return(
+        <header className='flex container'>
+            <p className='ff-secondary fs-h2 fw-bold letter-spacing-3'>#VanLife</p>
+            <nav className='flex'>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
+    )
+}
+export default Header
