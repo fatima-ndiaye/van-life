3148d2822acdd50e3259faab7193170594fd0699
fatima-xs <fatimanndiaye@gmail.com>
2023-06-23 08:17:24 +0100

    Styling NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,22 @@ import { NavLink,Link} from 'react-router-dom'
 
 function Header() 
 {
+    const activeStyle =
+    {
+        fontWeight:"bold",
+        color: "#161616"
+
+    }
     return(
         <header className='flex container'>
             <Link className="site-logo ff-secondary fs-h2 fw-bold letter-spacing-3 text-dark" to="/">#VanLife</Link>
             <nav className='flex'>
-                <NavLink to="/host">Host</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/vans">Vans</NavLink>
+                <NavLink to="/host" style={({isActive})=> isActive ?activeStyle:null}>Host</NavLink>
+                <NavLink to="/about" style={({ isActive }) => isActive ? activeStyle : null}>About</NavLink>
+                <NavLink to="/vans" style={({ isActive }) => isActive ? activeStyle : null}>Vans</NavLink>
             </nav>
         </header>
     )
 }
 export default Header
+
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from 'react-router-dom'
 function HostLayout()
 {
     return(
-        <>
+        <div className="container">
             <nav className='flex host-nav'>
                 <Link to="/host">Dashboard</Link>
                 <Link to="/host/income">Income</Link>
@@ -13,7 +13,7 @@ function HostLayout()
             </nav>
             <Outlet />
         
-        </>
+        </div>
     )
 }
 export default HostLayout
