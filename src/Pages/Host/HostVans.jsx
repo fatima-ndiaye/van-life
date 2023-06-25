5a1b50b06c244f44030b603c6921f8515fb1d5df
fatima-xs <fatimanndiaye@gmail.com>
2023-06-25 08:03:54 +0100

    Implementing Host Vans list page

--- src/Pages/Host/HostVans.jsx
+++ src/Pages/Host/HostVans.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 
 function HostVans() {
+    const [hostVans, setHostVans] = useState([])
+    useEffect(() => {
+        fetch('/api/host/vans')
+            .then(res => res.json())
+            .then(data =>setHostVans(data.vans))
+    }, [])
+
+    const hostVanEl = hostVans.map(hostVan =>{
+        return(
+            <Link to={`/host/vans/${hostVan.id}`} key={hostVan.id} className='host-van-link-tile text-dark'>
+                <div className='host-van-tile flex bg-light' key={hostVan.id}>
+                    <img src={hostVan.imageUrl}  className='host-van-img'/>
+                    <div className='host-van-info'>
+                        <h3 className='fs-h3'>{hostVan.name}</h3>
+                        <p>${hostVan.price}/day</p>
+                    </div>
+                </div>
+            </Link>
+
+            
+        )
+    })
     return (
-        <h1>Host Vans</h1>
+        <div className='van-host-list-container container flex'>
+            <h2>Your Listed Vans</h2>
+            <div className='van-host-list flex'>
+                {
+                    hostVans.length > 0 ?  hostVanEl 
+                    :
+                    <h2>Loading</h2>
+                }
+            </div>
+            
+        </div>
+        
     )
 }
 export default HostVans
