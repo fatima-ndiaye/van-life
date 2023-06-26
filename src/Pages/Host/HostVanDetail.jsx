862352f2f4729e74937e420a935efd60680fc437
fatima-xs <fatimanndiaye@gmail.com>
2023-06-26 10:23:14 +0100

    Add link to layout
    Adding .back-button to HostVanDetail.jsx

--- src/Pages/Host/HostVanDetail.jsx
+++ src/Pages/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 function HostVanDetail() 
 {
@@ -13,6 +13,9 @@ function HostVanDetail()
     },[params.id])
      console.log(hostVan)
     return (
+    <section>
+        <Link to=".." relative='path' className="back-button text-dark">&larr; <span>Back to all vans</span></Link>
+
         <div className='host-van-detail-container'>
          {
             hostVan ?
@@ -28,6 +31,7 @@ function HostVanDetail()
          }
 
         </div>
+    </section>
     )
 }
 export default HostVanDetail
