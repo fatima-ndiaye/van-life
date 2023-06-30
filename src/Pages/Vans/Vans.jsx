a6ed167652d6abdbbe6fa2b70504b0bee31eae62
fatima-xs <fatimanndiaye@gmail.com>
2023-06-30 08:40:00 +0100

    Update Van.jsx

--- src/Pages/Vans/Vans.jsx
+++ src/Pages/Vans/Vans.jsx
@@ -20,7 +20,7 @@ function Vans() {
     const vanEl = displayedVans.map(van =>{
         return (
         <div className='van-tile' key={van.id}>
-            <Link to={`/vans/${van.id}`} className='text-dark flex'>
+            <Link to={van.id} className='text-dark flex'>
                     <img src={van.imageUrl} className='van-img' />
                     <div className='van-info flex'>
                         <h3 className='van-name fs-h3'>{van.name}</h3>
