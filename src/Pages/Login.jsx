0d4d693e32a0adf5591b4ab6564f29239bb68607
fatima-xs <fatimanndiaye@gmail.com>
2023-07-03 15:42:55 +0100

    Update login-btn

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
                         value={loginFormData.password}
                     />
                 </div>
-                <button className="link-btn login-btn bg-accent">Log in</button>
+                <button className="link-btn login-btn bg-accent text-dark">Log in</button>
             </form>
         </div>
     )
