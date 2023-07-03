451782c0e4768a044882aaaecfcad71378507539
fatima-xs <fatimanndiaye@gmail.com>
2023-07-03 09:24:12 +0100

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useRouteError } from "react-router-dom"
 
 
 function Error() {
+    const error = useRouteError()
     return (
-        <h1>An error occurred!</h1>
+        <div className='container'>
+            <h2 className='fs-h3'>Error: {error.message}</h2>
+            <pre>{error.status} - {error.statusText}</pre>
+
+        </div>
     )
 }
 export default Error
