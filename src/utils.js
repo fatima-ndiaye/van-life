94fad173a22e2cc3bd950df2e477ffff78c8150d
fatima-xs <fatimanndiaye@gmail.com>
2023-07-05 08:11:24 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { redirect } from "./redirectUtils"
 
 export async function requireAuth() {
-    const isLoggedIn = false
+    const isLoggedIn = true
 
     if (!isLoggedIn) {
         throw redirect("/login?message=You must log in first")
