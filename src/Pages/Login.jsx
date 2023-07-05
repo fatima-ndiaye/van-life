b81dcd660f4494b3d2850832a8c0ca40f4097405
fatima-xs <fatimanndiaye@gmail.com>
2023-07-05 08:46:00 +0100

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
         setStatus("submitting")
         setError(null)
         loginUser(loginFormData)
-        .then(data => navigate("/host"))
+        .then(data => navigate("/host/vans"))
         .catch(err => setError(err))
         .finally(() => setStatus("idle"))
        
