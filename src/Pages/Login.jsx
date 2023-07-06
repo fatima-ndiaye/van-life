47c2d68bedb3df4ff5f4f7981e3180be0783ce41
fatima-xs <fatimanndiaye@gmail.com>
2023-07-06 08:52:34 +0100

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { useLoaderData, useNavigate, Form } from "react-router-dom"
+import { useLoaderData, useActionData, useNavigation, Form } from "react-router-dom"
 import {loginUser} from '../api'
 import { redirect } from "../redirectUtils"
 
@@ -12,23 +12,30 @@ export async function action({ request })
     const formData = await  request.formData()
     const email = formData.get("email")
     const password = formData.get("password")
-    const data = await loginUser({ email, password })
-    localStorage.setItem("loggedin", true)
-    return redirect("/host")
+    try
+    {
+        const data = await loginUser({ email, password })
+        localStorage.setItem("loggedin", true)
+        return redirect("/host")
+    }
+    catch(error)
+    {
+        return error.message
+    }
+
 }
 
 export default function Login() {
-    const [status, setStatus] = useState("idle")
-    const [error, setError] = useState(null)
     const message = useLoaderData()
-    const navigate = useNavigate()
+    const errorMessage = useActionData()
+    const navigation = useNavigation()
   
     return (
         <div className="login-container container flex">
             <h2 className="fs-h2 fw-light">Sign in to your account</h2>
             {message && <h3 className="">{message}</h3>}
-            {error && <h3 className="">{error.message}</h3>}
-            <Form className="login-form flex" method="post">
+            {errorMessage && <h3 className="">{errorMessage}</h3>}
+            <Form className="login-form flex" method="post" replace>
                 <div className="input-container flex">
                     <input
                         name="email"
@@ -41,8 +48,8 @@ export default function Login() {
                         placeholder="Password"
                     />
                 </div>
-                <button className="link-btn login-btn bg-accent text-dark" disabled={status === "submitting"}>
-                    {status === "submitting"? "Logging in...": "Log in"}
+                <button className="link-btn login-btn bg-accent text-dark" disabled={navigation.state === "submitting"}>
+                    {navigation.state === "submitting"? "Logging in...": "Log in"}
                 </button>
             </Form>
         </div>
